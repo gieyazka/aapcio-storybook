2f95482259dd6b07a33512ff5422e2d5c6f4d46e
pokkate.e <pokkate.e@aapico.com>
2022-07-08 14:09:01 +0700

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import mdx from 'vite-plugin-mdx'
 // https://vitejs.dev/config/
+const options = {
+  // See https://mdxjs.com/advanced/plugins
+  remarkPlugins: [
+    // E.g. `remark-frontmatter`
+  ],
+  rehypePlugins: [],
+}
+
 export default defineConfig({
   plugins: [react()]
 })
